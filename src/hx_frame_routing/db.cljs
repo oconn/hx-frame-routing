ba0440af07d05582f816ac74f7d901210d225db3
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2019-07-30 05:36:29 -0400

    Add state accessors

--- src/hx_frame_routing/db.cljs
+++ src/hx_frame_routing/db.cljs
@@ -1,11 +1,28 @@
 (ns hx-frame-routing.db
   (:require [cljs.spec.alpha :as s]))
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; State
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def initial-state
   {:hx-frame-router {:route nil
                      :route-params nil
                      :route-query nil
                      :initialized false}})
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Getters / Setters
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn get-route-key [db] (get-in db [:hx-frame-router :route]))
+(defn get-route-params [db] (get-in db [:hx-frame-router :route-params]))
+(defn get-route-query [db] (get-in db [:hx-frame-router :route-query]))
+(defn get-initialized [db] (get-in db [:hx-frame-router :initialized]))
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Specs
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (s/def :router/initialized boolean?)
 (s/def :router/route (s/nilable keyword?))
