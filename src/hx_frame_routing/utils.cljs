b2982ecc8cbd9f38741d296c978952e90a4ece73
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2019-09-12 08:55:51 -0400

    Add route matching helper

--- src/hx_frame_routing/utils.cljs
+++ src/hx_frame_routing/utils.cljs
@@ -1,8 +1,14 @@
 (ns hx-frame-routing.utils
-  (:require [bidi.bidi :as bidi]))
+  (:require [bidi.bidi :as bidi]
+            [goog.object :as gobj]))
 
 (defonce app-routes (atom nil))
 
 (defn path-for
   [route & args]
   (apply (partial bidi/path-for @app-routes route) args))
+
+(defn get-active-route
+  []
+  (bidi/match-route @app-routes
+                    (gobj/getValueByKeys js/window "location" "pathname")))
